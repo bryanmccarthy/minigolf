d23f6a6c9eb7ed72129c85fc216bc40a2c1a83f7
Bryan McCarthy <bryanmccarthy2@gmail.com>
2024-01-18 12:46:24 +0000

    home screen style

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,10 +10,10 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="flex flex-col bg-gradient-to-b from-blue-200 to-blue-100 h-[calc(100dvh)]">
-      <p className="text-center py-8 text-4xl text-green-500 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">minigolf</p>
+      <p className="text-center py-6 text-5xl font-light text-black drop-shadow">minigolf</p>
       <div className="flex flex-col justify-center items-center pt-4">
         <div className="w-40 h-40 p-8 m-1 bg-green-500 rounded shadow-xl">
-          <div className="w-8 h-8 rounded-full bg-gray-100 border border-gray-100 shadow-inner"></div>
+          <div className="w-8 h-8 rounded-full bg-gray-100 border border-gray-100 shadow-inner shadow-gray-400"></div>
         </div>
         <div className="w-32 h-32 p-8 m-1 bg-green-500 rounded ml-20 shadow-lg">
         </div>
@@ -21,8 +21,8 @@ export default function Index() {
         </div>
       </div>
       <div className="flex justify-center items-center pt-16">
-        <button className="bg-blue-400 mx-4 w-28 h-10 text-white rounded shadow-sm">Sign Up</button>
-        <button className="bg-orange-400 mx-8 w-28 h-10 text-white rounded shadow-sm">Guest</button>
+        <button className="bg-slate-700 mx-4 w-28 h-10 text-white rounded-md shadow">Sign In</button>
+        <button className="bg-orange-500 mx-8 w-28 h-10 text-white rounded-md shadow">Guest</button>
       </div>
     </div>
   );
