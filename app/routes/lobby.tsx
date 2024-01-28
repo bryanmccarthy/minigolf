d85db9f21a91c549232fe9f9d8ebf7ae679d34ef
Bryan McCarthy <bryanmccarthy2@gmail.com>
2024-01-28 10:24:02 +0000

    remove home btn in lobby

--- app/routes/lobby.tsx
+++ app/routes/lobby.tsx
@@ -38,16 +38,6 @@ export default function Lobby() {
       <div className="flex flex-col bg-gradient-to-b from-blue-300 to-blue-200 h-[calc(100dvh)]">
         <div className="flex">
           <div className="flex flex-col gap-2 p-2">
-            {/* TODO: move to settings popup/dropdown */}
-            <div className="py-2">
-              <Link to={"/"} className="flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-                </svg>
-                <p className="text-xl font-light">home</p>
-              </Link>
-            </div>
-
             <div className="flex flex-col">
               <div className="flex items-end gap-2">
                 <p className="text-2xl font-semibold text-black">Lobby (3/4)</p>
@@ -62,12 +52,7 @@ export default function Lobby() {
                 <div className="flex items-center h-12 bg-slate-700 rounded shadow-lg">
                   <p className="text-white text-lg px-2 truncate">{ session && session.user ? session.user.user_metadata.username : "Guest" } (Me)</p>
                 </div>
-                <div className="flex items-center h-12 bg-white rounded shadow-lg">
-                  <p className="text-black text-lg px-2 truncate">Player 2</p>
-                </div>
-                <div className="flex items-center h-12 bg-white rounded shadow-lg">
-                  <p className="text-black text-lg px-2 truncate">Player 3</p>
-                </div>
+                {/* TODO: Display party members */}
               </div>
             </div>
             <div className="flex flex-col pt-2">
