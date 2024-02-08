680203056370a6d455ca053eb04060732acc744a
Bryan McCarthy <bryanmccarthy2@gmail.com>
2024-02-08 14:48:33 +0000

    rm newlob just use new component

--- app/routes/lobby.tsx
+++ app/routes/lobby.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import { Link, useOutletContext, useNavigate } from "@remix-run/react";
 import type { OutletContext, Profile, Party, Message } from "../utils/types";
 import useProfile from "../hooks/useProfile";
+import InvitePane from "../components/InvitePane";
 
 const courses = [
   "Practice",
@@ -183,15 +184,9 @@ export default function Lobby() {
         <div className="relative flex">
           {/* Invite Pane */}
           { showInvitePane &&
-            <div className="absolute flex flex-col items-center w-44 h-32 top-2 left-48 rounded shadow-lg bg-white">
-              <button className="ml-auto p-2" onClick={handleToggleInvitePane}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                </svg>
-              </button>
-              <input placeholder="username" className="w-10/12 h-8 px-2 my-1 border-2 border-black rounded text-black outline-none" />
-              <button className="w-10/12 h-8 my-1 bg-black text-white rounded">Invite</button>
-            </div>
+            <InvitePane
+              close={handleToggleInvitePane}
+            />
           }
           <div className="flex flex-col gap-2 p-2 w-48">
             <div className="flex flex-col">
