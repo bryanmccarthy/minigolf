68ba4c47fa05713dd3a96c6fb2ca65cf23cc99f5
Bryan McCarthy <bryanmccarthy2@gmail.com>
2024-02-11 11:08:20 +0000

    profile label

--- app/routes/lobby.tsx
+++ app/routes/lobby.tsx
@@ -486,11 +486,14 @@ export default function Lobby() {
             }
             { showUserMenu &&
               <div className="absolute flex flex-col top-0 right-0 w-56 m-2 rounded shadow-lg bg-white">
-                <button className="text-black text-xl font-semibold ml-auto p-2" onClick={handleToggleUserMenu}>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                  </svg>
-                </button>
+                <div className="flex justify-between items-center p-2">
+                  <p className="text-lg font-light text-black px-2">Profile</p>
+                  <button onClick={handleToggleUserMenu}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>
                 <div className="flex flex-col gap-3 p-3 mt-auto">
                   <div className="flex items-center text-neutral-700 bg-neutral-100 px-2 py-1 rounded font-semibold truncate">
                     <input className="w-40 bg-neutral-100 outline-none" value={usernameEdit ? usernameEdit : profile.display_name} onChange={(e) => handleUsernameInputChange(e)} />
