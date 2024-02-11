8db18e8bcc2e8a89676baeeca59e44c4ca7ca057
Bryan McCarthy <bryanmccarthy2@gmail.com>
2024-02-11 11:04:48 +0000

    invite label

--- app/components/InvitePane.tsx
+++ app/components/InvitePane.tsx
@@ -34,11 +34,14 @@ export default function InvitePane({ displayName, partyId, partyMembers, supabas
 
 	return (
 	    <div className="absolute flex flex-col items-center w-44 h-32 top-2 left-48 rounded shadow-lg bg-white">
-	      <button className="ml-auto p-2" onClick={close}>
-	        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-	          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-	        </svg>
-	      </button>
+        <div className="flex w-full justify-between items-center p-2">
+          <p className="font-light text-lg px-2">Invite</p>
+          <button onClick={close}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+          </button>
+        </div>
 	      <input 
           value={username} 
           placeholder="username" 
