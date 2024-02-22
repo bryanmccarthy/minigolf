b155fc44a1a4a2406d102f5f098d0df3790b52bc
Bryan McCarthy <bryanmccarthy2@gmail.com>
2024-02-22 23:24:28 +0000

    chat toggle & some styles

--- app/routes/lobby.tsx
+++ app/routes/lobby.tsx
@@ -30,6 +30,7 @@ export default function Lobby() {
   const [usernameEdit, setUsernameEdit] = useState("");
   const [showPartyMemberEdit, setShowPartyMemberEdit] = useState(false);
   const [selectedMember, setSelectedMember] = useState<Profile | null>(null);
+  const [showPartyMessagesBox, setShowPartyMessagesBox] = useState(false);
   const [message, setMessage] = useState("");
   const [invites, setInvites] = useState<Invite[]>([]);
   const messagesBoxRef = useRef<HTMLDivElement>(null);
@@ -505,7 +506,7 @@ export default function Lobby() {
 
   return (
     <>
-      <div className="flex flex-col bg-gradient-to-b from-blue-300 to-blue-200 h-[calc(100dvh)]">
+      <div className="flex flex-col bg-sky-300 h-[calc(100dvh)]">
         <div className="relative flex">
           {/* Invite Pane */}
           { showInvitePane &&
@@ -646,6 +647,17 @@ export default function Lobby() {
           </div>
           <div className="relative flex flex-col gap-2 p-2 ml-auto">
             <div className="flex items-center ml-auto gap-2">
+              <button className="cursor-pointer" onClick={() => setShowPartyMessagesBox(!showPartyMessagesBox)}>
+                { showPartyMessagesBox ?
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                    <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm.53 5.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l1.72-1.72v5.69a.75.75 0 0 0 1.5 0v-5.69l1.72 1.72a.75.75 0 1 0 1.06-1.06l-3-3Z" clipRule="evenodd" />
+                  </svg>
+                :                
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                    <path fillRule="evenodd" d="M4.848 2.771A49.144 49.144 0 0 1 12 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97-1.94.284-3.916.455-5.922.505a.39.39 0 0 0-.266.112L8.78 21.53A.75.75 0 0 1 7.5 21v-3.955a48.842 48.842 0 0 1-2.652-.316c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97Z" clipRule="evenodd" />
+                  </svg>
+                }
+              </button>
               <div className="relative cursor-pointer" onClick={() => setShowNotifications(true)}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-7 h-7">
                   <path fillRule="evenodd" d="M5.25 9a6.75 6.75 0 0 1 13.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 0 1-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 1 1-7.48 0 24.585 24.585 0 0 1-4.831-1.244.75.75 0 0 1-.298-1.205A8.217 8.217 0 0 0 5.25 9.75V9Zm4.502 8.9a2.25 2.25 0 1 0 4.496 0 25.057 25.057 0 0 1-4.496 0Z" clipRule="evenodd" />
@@ -660,7 +672,7 @@ export default function Lobby() {
                 <p className="text-black text-2xl font-extrabold">{ profile.display_name[0] }</p>
               </div>
             </div>
-            { party &&
+            { party && showPartyMessagesBox &&
               <>
                 <PartyMessagesBox partyMessages={partyMessages} profile={profile} messagesBoxRef={messagesBoxRef} />
                 <div className="flex gap-1 w-80">
